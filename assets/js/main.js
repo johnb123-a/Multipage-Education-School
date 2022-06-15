d3dfe960ccafa0a9ae65cef4bc28c42c9c153035
John Barreto <john.barreto@api-ux.com>
2022-06-15 16:53:51 -0500

    faqs toggle clas open

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,16 @@
 
 window.addEventListener('scroll', () => {
     document.querySelector('.nav').classList.toggle('window-scroll', window.scrollY > 0)
-})
+});
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.container__faq');
+
+faqs.forEach(faq => {
+
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+    });
+
+});
