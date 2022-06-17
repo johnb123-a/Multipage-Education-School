d0f3ced4615c786fb9c8ce878b52ea40a528fee4
John Barreto <john.barreto@api-ux.com>
2022-06-17 12:17:08 -0500

    add toggle function in navbar dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,21 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
 });
+
+
+// Show/Hide nav menu
+const menu = document.querySelector('.container__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+});
